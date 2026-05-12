f5fd9e309776922f9d45749959f93a136cb32487
JorenVanGansbergh <Joren.VAN.GANSBERGHE@student.howest.be>
2026-05-12 13:35:20 +0200

    Added files that should not be committed in gitignore, added a script to set up symlinks to the eDNA data, and customised navaseq_slurm.sh to work in my directory

--- run/novaseq_slurm.sh
+++ run/novaseq_slurm.sh
@@ -2,38 +2,39 @@
 
 # Set the allocation to be charged for this job
 # not required if you have set a default allocation
-#SBATCH -A project-allocation
+#SBATCH -A naiss2025-22-761
 
 # The name of the script is myjob
-#SBATCH -J openarms-novaseq
+#SBATCH -J openarms-joren-18S-2022-2025
 
 # The partition
-#SBATCH -p main
+#SBATCH -p main ##main is the part of the cluster where you run it?, main is 24h max
 
 # 24 hours wall clock time will be given to this job
-#SBATCH -t 20:00:00
+#SBATCH -t 24:00:00
 
 # Number of nodes
 #SBATCH --nodes=1
 
-#SBATCH --mail-user=user@email.com
+#SBATCH --mail-user=Joren.VAN.GANSBERGHE@student.howest.be
 #SBATCH --mail-type=ALL
 
 ## Set the names for the error and output files. 
 ## It can be smart to set a path to these to your project directory, which you can do by adding that path right after the '=' sign
-#SBATCH --error=/PATH/TO/open-arms/sbatch_jobs/job.%J.err
-#SBATCH --output=/PATH/TO/open-arms/sbatch_jobs/job.%J.out
+#SBATCH --error=/cfs/klemming/projects/supr/naiss2025-23-46/Joren/Project_18S/sbatch_jobs/job.%J.err
+#SBATCH --output=/cfs/klemming/projects/supr/naiss2025-23-46/Joren/Project_18S/sbatch_jobs/job.%J.out
 
 ## module purge, then load necessary modules
-## module purge 
-## module load ...
-## module load miniconda module
 
-WORKDIR=/PATH/TO/open-arms;
-TMPDIR=/PATH/TO/TEMP_DIR;
+module purge 
+module load PDC/24.11
+module load apptainer/1.4.0-cpeGNU-24.11
+
+WORKDIR=/cfs/klemming/projects/supr/naiss2025-23-46/Joren/Project_18S;
+TMPDIR=/cfs/klemming/scratch/j/jorenvgb;
 SANDBOX="$TMPDIR/temparms-sandbox"
 
-apptainer exec --bind "$WORKDIR":/work --bind "$TMPDIR/conda_envs":/envs --bind /ABSOLUTE/PATH/TO/SYMLINKED/FILES/SOURCE:/ABSOLUTE/PATH/TO/SYMLINKED/FILES/SOURCE "$SANDBOX" bash /work/run/novaseq_run.sh
+apptainer exec --bind "$WORKDIR":/work --bind "$TMPDIR/conda_envs":/envs --bind /cfs/klemming/projects/supr/naiss2025-23-46/ARMS_Genoscope_data_full/projet_DBB/ARMS-Macro:/cfs/klemming/projects/supr/naiss2025-23-46/ARMS_Genoscope_data_full/projet_DBB/ARMS-Macro "$SANDBOX" bash /work/run/novaseq_run.sh
 
 
 
