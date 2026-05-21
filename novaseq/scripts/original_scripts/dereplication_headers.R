6d30829d2d7ebd88083dcaa3d8ebfa398ceaa30a
JorenVanGansbergh <Joren.VAN.GANSBERGHE@student.howest.be>
2026-05-21 16:56:39 +0200

    dereplication_headeers.R - now compatible with 18S

--- novaseq/scripts/original_scripts/dereplication_headers.R
+++ novaseq/scripts/original_scripts/dereplication_headers.R
@@ -3,11 +3,11 @@
 # For clustering ASVs into MOTUs using swarm, an ASV fasta with headers containing the total abundance of each is required.
 # Here, we generate these new headers for the fasta files which will be used as input for swarm.
 
-### COI ###
+### 18S ###
 
-# Read the non-contaminant COI ASV count table 
+# Read the non-contaminant 18S ASV count table 
 
-ASV_counts <- read.table(file = "novaseq/COI/blank_corr/asv_no_contaminants_COI.txt", sep = "\t", row.names = 1, header = T)
+ASV_counts <- read.table(file = "novaseq/18S/blank_corr/asv_no_contaminants_18S.txt", sep = "\t", row.names = 1, header = T)
 
 # Count total read abundances per ASV 
 
@@ -17,7 +17,7 @@ ASV_sums <-rowSums(ASV_counts)
 
 seqnames <- paste0(">", paste(rownames(ASV_counts), ASV_sums, sep="_"))
 
-write.table(seqnames, "novaseq/COI/ASV_dereplicated.txt", sep="\t", col.names = F, row.names = F, quote = F)
+write.table(seqnames, "novaseq/18S/ASV_dereplicated.txt", sep="\t", col.names = F, row.names = F, quote = F)
 
-swarm_dir <- "novaseq/COI/swarm"
+swarm_dir <- "novaseq/18S/swarm"
 if(!dir.exists(swarm_dir)) dir.create(swarm_dir)
