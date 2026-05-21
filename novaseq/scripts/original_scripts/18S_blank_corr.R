242d4b571bbbbbcf00ef6bf08c61c7a24216a1b9
JorenVanGansbergh <Joren.VAN.GANSBERGHE@student.howest.be>
2026-05-21 16:38:19 +0200

    18S_blank_corr.R - updated COI_blank_corr.R to handle 18S data

--- novaseq/scripts/original_scripts/18S_blank_corr.R
+++ novaseq/scripts/original_scripts/18S_blank_corr.R
@@ -1,34 +1,22 @@
 #!/usr/bin/env Rscript
 
-### COI ###
+### 18S ###
 
-output_dir <- "novaseq/COI/blank_corr"
+output_dir <- "novaseq/18S/blank_corr"
 
 if(!dir.exists(output_dir)) dir.create(output_dir)
 
-# read COI sample summary and extract negative control IDs
+# read 18S sample summary and extract negative control IDs
 
-coi_summary <- read.csv("metadata/generated_meta/koster_coi_2022_2023.csv", 
+18S_summary <- read.csv("metadata/generated_meta/18S_batch3.4.5.6-metadata.csv", 
                         sep = ",", header = TRUE)
-coi_negative <- c(unique(coi_summary$PCR_negative_control_Code_1), unique(coi_summary$PCR_negative_control_Code_2))
+18S_negative <- c(unique(18S_summary$PCR_negative_control_Code_1), unique(18S_summary$PCR_negative_control_Code_2))
 
 # Read ASV count table (output from dada2)
 
-asv_table <- read.table("novaseq/COI/COI_ASV_counts_nosingle.txt",
+asv_table <- read.table("novaseq/18S/18S_ASV_counts_nosingle.txt",
                         sep = "\t", header = T, row.names = 1,
                         as.is = T, check.names = F)
-
-# Read list of ASVs which remained after NUMT removal
-
-asv_list <- read.table("novaseq/COI/pseudo/nonpseudo.combined.names.txt", sep =  "\t")
-
-# Remove ">" in asv_list
-
-asv_list[,1] <- gsub(">", "", asv_list[,1])
-
-# Subset asv_table to non-numt ASVs
-
-asv_table <- subset(asv_table, rownames(asv_table) %in% asv_list[,1])
 
 # Subset rows where the ASV read count in the blank / negative samples  exceeds 10 % of an ASVs total read count 
 # before combining the tables using rbind, add a column with ASV IDs. 
@@ -36,7 +24,7 @@ asv_table <- subset(asv_table, rownames(asv_table) %in% asv_list[,1])
 
 blank_list <- list()
 
-for (id in coi_negative) {
+for (id in 18S_negative) {
   subset_id <- asv_table[grep(id, colnames(asv_table))]
 
   negative_id <- subset(asv_table, subset_id > 0.1 * rowSums(asv_table))
@@ -59,11 +47,11 @@ asv_no_contams <- asv_table[!(asv_table$ASV_ID %in% blanks$ASV_ID),]
 
 # Write table of potential contaminant ASVs and ASV count table devoid of contaminants
 
-write.table(blanks, file = file.path(output_dir, "asv_contaminants_COI.txt"), sep="\t", row.names = F)
+write.table(blanks, file = file.path(output_dir, "asv_contaminants_18S.txt"), sep="\t", row.names = F)
 
-write.table(asv_no_contams, file = file.path(output_dir, "asv_no_contaminants_COI.txt"), sep="\t", row.names = F)
+write.table(asv_no_contams, file = file.path(output_dir, "asv_no_contaminants_18S.txt"), sep="\t", row.names = F)
 
 # Write headers of non-contaminant asvs to file to subset the corresponding sequences of non-NUMT fasta file later on
 
 no_contam_headers <- paste0(">",asv_no_contams$ASV_ID)
-write.table(no_contam_headers, file = file.path(output_dir, "no_contam_headers_COI.txt"), sep="\t", row.names = F,quote=F,col.names = F)
+write.table(no_contam_headers, file = file.path(output_dir, "no_contam_headers_18S.txt"), sep="\t", row.names = F,quote=F,col.names = F)
